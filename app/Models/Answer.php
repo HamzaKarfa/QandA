a9cf5cf21612a165e9a53d36319ad33e4c2023be
hamza karfa <hamza.karfa@gmail.com>
2021-04-22 18:16:36 +0200

    init project

---
+++ app/Models/Answer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    use HasFactory;
+    protected $fillable = ['label', 'is_valid'];
+
+    public $timestamps = false;
+
+    public function question(){
+      return $this->belongsTo('App\Models\Question');
+    }
+}
